5518c01b28a56057ed61f67c67bf8a275d62529f
khazaddoom <31121986g@gmail.com>
2018-07-04 18:14:56 +0530

    Learning BroswerSync

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var watch = require('gulp-watch');
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    });
+
+    gulp.watch('app/*.html', function() {
+
+        console.log('hello');
+
+    });
+
+});
